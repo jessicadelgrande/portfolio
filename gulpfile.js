60424bce35fa79604b4f32c496968580f1355f28
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-03-22 17:24:36 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,26 @@ const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('js', () => {
-    browserify('src/app.js', {debug: true})
+gulp.task('styles', () => {
+    return gulp.src('./dev/styles/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error',notify.onError({
+            message: "Error: <%= error.message %>",
+            title: 'Error in CSS 💀'
+        })))
+    .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/styles'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('javascript', () => {
+    browserify('dev/app.js', {debug: true})
         .transform('babelify', {
             sourceMaps: true,
             presets: ['es2015','react']
@@ -21,20 +38,23 @@ gulp.task('js', () => {
         }))
         .pipe(source('app.js'))
         .pipe(buffer())
-        .pipe(gulp.dest('public/'))
+        .pipe(gulp.dest('./public'))
         .pipe(reload({stream:true}));
 });
 
-gulp.task('bs', () => {
+gulp.task('browserSync', () => {
     browserSync.init({
         server: {
             baseDir: './'
-        }
+        },
+        middleware: [historyApiFallback()]
     });
 });
 
-
-gulp.task('default', ['js','bs'], () => {
-    gulp.watch('src/**/*.js',['js']);
-    gulp.watch('./public/style.css',reload);
+gulp.task('watch', () => {
+    gulp.watch('./dev/**/*.js', ['javascript']);
+    gulp.watch('./dev/styles/**/*.scss', ['styles']);
+    gulp.watch('./*.html', reload);
 });
+
+gulp.task('default', ['browserSync', 'styles', 'javascript', 'watch'])
